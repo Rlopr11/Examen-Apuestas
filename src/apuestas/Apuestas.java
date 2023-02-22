1045b71c0885b226da2973562e139aeeb26def60
HGH011 <114875813+HGH011@users.noreply.github.com>
2023-02-22 12:08:51 +0100

    He aplicado el patron rename a la variable miApuesta.

--- src/apuestas/Apuestas.java
+++ src/apuestas/Apuestas.java
@@ -7,24 +7,24 @@ package apuestas;
 public class Apuestas {
 
     public static void main(String[] args) {
-        Apuesta miApuesta;
+        Apuesta laApuesta;
         int mi_dinero;
 
-        miApuesta = new Apuesta(1000, 4, 2);
+        laApuesta = new Apuesta(1000, 4, 2);
         try {
             System.out.println("Apostando...");
-            miApuesta.apostar(25);
+            laApuesta.apostar(25);
         } catch (Exception e) {
             System.out.println("Fallo al realizar la Apuesta");
         }
 
         try {
             System.out.println("Intento cobrar apuesta segun el resultado del partido");
-            miApuesta.cobrar_apuesta(2, 3);
+            laApuesta.cobrar_apuesta(2, 3);
         } catch (Exception e) {
             System.out.println("Fallo al cobrar la apuesta");
         }
-        mi_dinero = miApuesta.getDinero_disp();
+        mi_dinero = laApuesta.getDinero_disp();
         System.out.println("El dinero que tengo tras las apuestas es " + mi_dinero);
     }
 
